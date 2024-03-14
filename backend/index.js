26b9f44fada19ff39d80c84b964733897b67e2ad
dendywool <zaharkhachaturyan@gmail.com>
2024-03-14 13:37:27 +0100

    realized reg and log

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
 
 dotenv.config()
@@ -12,7 +13,9 @@ mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected!"))
     .catch(err => console.log(err));
 
-app.use("/api/pins", pinRoute)
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
 
 app.listen(8800, () => {
     console.log('Backend server is running!')
